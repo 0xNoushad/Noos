a281706bb2c6964ab1e0468113bf1143779ebe14
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2024-03-19 16:19:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const API_KEY = "b9b62ac490b94a419f5ff24569a46432";
 const url = "https://newsapi.org/v2/everything?q=";
 
-window.addEventListener("load", () => fetchNews("India"));
+window.addEventListener("load", () => fetchNews("finance")); // Default to 'finance' category on page load
 
 function reload() {
     window.location.reload();
 }
 
 async function fetchNews(query) {
-    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data = await res.json();
-    bindData(data.articles);
+    try {
+        const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await res.json();
+        bindData(data.articles);
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
 }
 
 function bindData(articles) {
@@ -33,7 +40,7 @@ function fillDataInCard(cardClone, article) {
     const newsSource = cardClone.querySelector("#news-source");
     const newsDesc = cardClone.querySelector("#news-desc");
 
-    newsImg.src = article.urlToImage;
+    newsImg.src = article.urlToImage; // Update image source
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
 
@@ -52,7 +59,7 @@ let curSelectedNav = null;
 function onNavItemClick(id) {
     fetchNews(id);
     const navItem = document.getElementById(id);
-    curSelectedNav?.classList.remove("active");
+    if (curSelectedNav) curSelectedNav.classList.remove("active");
     curSelectedNav = navItem;
     curSelectedNav.classList.add("active");
 }
@@ -64,6 +71,6 @@ searchButton.addEventListener("click", () => {
     const query = searchText.value;
     if (!query) return;
     fetchNews(query);
-    curSelectedNav?.classList.remove("active");
+    if (curSelectedNav) curSelectedNav.classList.remove("active");
     curSelectedNav = null;
 });
