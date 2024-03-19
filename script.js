dbd74b6d744f347caf871f9b53d9a26baaf0f34c
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2024-03-19 15:54:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+ const API_KEY = "b9b62ac490b94a419f5ff24569a46432";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
